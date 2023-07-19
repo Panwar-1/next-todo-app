a895af7cf3dfbaf06802a733efb4c3ecdd112088
“Dharmendra <dharmendra.singh@napses.com>
2023-07-19 22:36:23 +0530

    added file structure

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const index = () => {
+  return (
+    <div>
+        <Head>
+        <link href="https://api.mapbox.com/mapbox-gl-js/v2.5.0/mapbox-gl.css" rel="stylesheet"></link>
+        <link rel="favicon" href="/images/streamAlive-favicon.png" />
+        <title>Todo App</title>
+        </Head>
+
+          Hey HI Welcome To the main Page
+    </div>
+  )
+}
+
+export default index;
